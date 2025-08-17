8e5f127b421f6d679f634fea442595d09870d5ef
Rob Rust <bobby.rust121@gmail.com>
2025-08-17 00:23:53 -0400

    add tabs to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,9 @@ import { CirclePlus, Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import JobApplicationCard from "./components/ui/job-application-card/job-application-card";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { ApplicationStatus } from "@/types/enums";
+import App from "next/app";
 
 export default function Home() {
   const router = useRouter();
@@ -32,16 +35,29 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center gap-5">
         <div className="flex w-full justify-between">
           <div>
-            <ul className="flex gap-5">
-              <li>Overview</li>
-              <li>Rejections</li>
-              <li>Offers</li>
-              <li>Interviews</li>
-            </ul>
+            <Tabs defaultValue="overview" className="w-[400px]">
+              <TabsList>
+                <TabsTrigger value="Overview">Overview</TabsTrigger>
+                <TabsTrigger value={ApplicationStatus.RECEIVED_OFFER}>Offers</TabsTrigger>
+                <TabsTrigger value={ApplicationStatus.REJECTED}>Rejections</TabsTrigger>
+                <TabsTrigger value={ApplicationStatus.UNDER_CONSIDERATION}>Under Consideration</TabsTrigger>
+                <TabsTrigger value={ApplicationStatus.INTERVIEWING}>Interviewing</TabsTrigger>
+                <TabsTrigger value={ApplicationStatus.INTERVIEWED}>Interviewed</TabsTrigger>
+                <TabsTrigger value={ApplicationStatus.SHORTLISTED}>Shortlisted</TabsTrigger>
+              </TabsList>
+            </Tabs>
           </div>
-          <Button size="lg" onClick={() => { router.replace(decodeURIComponent(paths.home.applications.new.getHref())) }}>
-            <CirclePlus className="size-6" /> <span className="-translate-y-[1px]">New Application</span>
-          </Button>
+          <div className="flex gap-5">
+            <Tabs defaultValue="grid">
+              <TabsList>
+                <TabsTrigger value="grid">Grid View</TabsTrigger>
+                <TabsTrigger value="table">Table View</TabsTrigger>
+              </TabsList>
+            </Tabs>
+            <Button size="lg" onClick={() => { router.replace(decodeURIComponent(paths.home.applications.new.getHref())) }}>
+              <CirclePlus className="size-6" /> <span className="-translate-y-[1px] -translate-x-1">New Application</span>
+            </Button>
+          </div>
         </div>
         <div className="flex justify-center items-center">
           {jobApplications.isLoading ? (
